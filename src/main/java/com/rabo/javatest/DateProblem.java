7aa7af1baa1c6d315134f449d1a22f69755d3ef8
priyam das <priyamdas1982@gmail.com>
2020-03-07 07:47:44 +0100

    baseline version 2

--- src/main/java/com/rabo/javatest/DateProblem.java
+++ src/main/java/com/rabo/javatest/DateProblem.java
@@ -1,50 +1,61 @@
 package com.rabo.javatest;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeParseException;
 import java.util.HashSet;
 
 public class DateProblem {
-
-
+	/*This method expects 2 inputs, startTime and endTime in the format of HH:mm:ss. It calculates the difference between the those and counts all occurrences of time with only 2 unique digits in it.
+	 * E.g. : if startTime=16:15:00 and endDate=17:00:00; then count is 2, i.e. : 16:16:11 and 16:16:16, with unique occurrences of of 1 and 6*/
 	public int count(String startTime, String endTime) {
 
-	 int iCount=0;
-		LocalDateTime startdateTime = LocalDateTime.parse("2018-05-05T" + startTime);
-		LocalDateTime enddateTime = LocalDateTime.parse("2018-05-05T" + endTime);
-		LocalDateTime intermediateTime = enddateTime;
-/* Using HashSet as it stores unique values*/
-		HashSet<Integer> hsUniqueSet = null;
-		int h1 = 0;
-		int h2 = 0;
-		int m1 = 0;
-		int m2 = 0;
-		int s1 = 0;
-		int s2 = 0;
-		while (intermediateTime.isAfter(startdateTime)||intermediateTime.equals(startdateTime) ) {
-
-			hsUniqueSet = new HashSet<Integer>();
-			/* Store all digits separately*/
-			h1 = intermediateTime.getHour() / 10;
-			h2 = intermediateTime.getHour() % 10;
-			m1 = intermediateTime.getMinute() / 10;
-			m2 = intermediateTime.getMinute() % 10;
-			s1 = intermediateTime.getSecond() / 10;
-			s2 = intermediateTime.getSecond() % 10;
-
-			hsUniqueSet.add(h1);
-			hsUniqueSet.add(h2);
-			hsUniqueSet.add(m1);
-			hsUniqueSet.add(m2);
-			hsUniqueSet.add(s1);
-			hsUniqueSet.add(s2);
-			// Find the exact scenario with 2 unique digits
-			if (hsUniqueSet.size() == 2) {
-				System.out.println(intermediateTime);
-				iCount++;
+		int iCount = 0;
+		// adding dummy dates to complete datetime format
+		try {
+			LocalDateTime startdateTime = LocalDateTime.parse("2018-05-05T" + startTime);
+			LocalDateTime enddateTime = LocalDateTime.parse("2018-05-05T" + endTime);
+			if (enddateTime.isBefore(startdateTime)) {
+				enddateTime = LocalDateTime.parse("2018-05-06T" + endTime);
 			}
-			intermediateTime = intermediateTime.minusSeconds(1);
-		}
+			LocalDateTime intermediateTime = enddateTime;
+			/* Using HashSet as it stores unique values */
+			HashSet<Integer> hsUniqueSet = null;
+			int h1 = 0;
+			int h2 = 0;
+			int m1 = 0;
+			int m2 = 0;
+			int s1 = 0;
+			int s2 = 0;
+			while (intermediateTime.isAfter(startdateTime) || intermediateTime.equals(startdateTime)) {
 
-		return iCount;
+				hsUniqueSet = new HashSet<Integer>();
+				/* Store all digits separately */
+				h1 = intermediateTime.getHour() / 10;
+				h2 = intermediateTime.getHour() % 10;
+				m1 = intermediateTime.getMinute() / 10;
+				m2 = intermediateTime.getMinute() % 10;
+				s1 = intermediateTime.getSecond() / 10;
+				s2 = intermediateTime.getSecond() % 10;
+
+				hsUniqueSet.add(h1);
+				hsUniqueSet.add(h2);
+				hsUniqueSet.add(m1);
+				hsUniqueSet.add(m2);
+				hsUniqueSet.add(s1);
+				hsUniqueSet.add(s2);
+				// Find the exact scenario with 2 unique digits
+				if (hsUniqueSet.size() == 2) {
+					System.out.println(intermediateTime);
+					iCount++;
+				}
+				intermediateTime = intermediateTime.minusSeconds(1);
+			}
+
+			return iCount;
+
+		} catch (DateTimeParseException de) {
+			//return -1 in case of time format error
+			return -1;
+		}
 	}
 }
--- src/test/java/com/rabo/javatest/TestDateProblem.java
+++ src/test/java/com/rabo/javatest/TestDateProblem.java
@@ -35,4 +35,25 @@ public class TestDateProblem {
 		
 		//fail("Not yet implemented");
 	}
+	@Test
+	public void testEndDateLessThanStartDateCase() {
+		int iCount =dp.count("23:00:00", "01:00:00");
+		assertEquals(104, iCount);
+		
+		//fail("Not yet implemented");
+	}
+	@Test
+	public void testEndDateEqStartDateCase() {
+		int iCount =dp.count("17:17:17", "17:17:17");
+		assertEquals(1, iCount);
+		
+		//fail("Not yet implemented");
+	}
+	@Test
+	public void testInvalidDateFormatCase() {
+		int iCount =dp.count("231:00:00", "01:00:00");
+		assertEquals(-1, iCount);
+		
+		//fail("Not yet implemented");
+	}
 }
